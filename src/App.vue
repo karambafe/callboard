3e5143cd882144d8cd11c8059b5b5e0004b3b24a
Vitaly <karambafe@yahoo.com>
2017-03-19 19:37:51 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+
+<style lang="scss">
+  @import "assets/scss/main.scss";
+</style>
