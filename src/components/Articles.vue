a41ce3c58a96eeb1a5b10f62f4257fe93cc5e4f6
Vitaly <karambafe@yahoo.com>
2017-03-27 01:38:37 +0300

    find articles

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -2,8 +2,12 @@
   <div class="articles">
     <md-toolbar>
       <h2 class="md-title" style="flex: 1">Статьи</h2>
+      <md-input-container>
+        <label>Поиск</label>
+        <md-input v-model="filterKey"></md-input>
+      </md-input-container>
     </md-toolbar>
-    <md-card md-with-hover v-for="article in articles">
+    <md-card md-with-hover v-for="article in filteredData" :key="article.id">
       <md-card-header>
         <div class="md-title">{{ article.title }}</div>
       </md-card-header>
@@ -18,27 +22,48 @@
 </template>
 
 <script>
-export default {
-  name: 'articles',
-  data() {
-    return {
-      endpoint: 'https://jsonplaceholder.typicode.com/posts',
-      articles: [],
-    };
-  },
-  methods: {
-    getAllPOst() {
-      this.$http.get(this.endpoint).then(function (response) {
-        this.articles = response.data;
-        console.log(this.articles);
-      }, function (error) {
-        // ошибка
-      })
+  export default {
+    name: 'articles',
+    data() {
+      return {
+        endpoint: 'https://jsonplaceholder.typicode.com/posts',
+        articles: [],
+        filterKey:'',
+      };
     },
-  },
-  created() {
-    this.getAllPOst();
-  },
-};
+    methods: {
+      getAllPOst() {
+        const options ={
+          params: {
+            _limit: 10,
+          }
+        };
+        this.$http.get(this.endpoint, options).then(function (response) {
+          this.articles = response.data;
+          console.log(this.articles);
+        }, function (error) {
+          // ошибка
+        })
+      },
+    },
+    computed: {
+      filteredData: function () {
+        var filterKey = this.filterKey && this.filterKey.toLowerCase();
+        var data = this.articles;
+        if (filterKey) {
+          data = data.filter(function (row) {
+            return Object.keys(row).some(function (key) {
+              return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+            })
+          })
+        }
+
+        return data;
+      }
+    },
+    created() {
+      this.getAllPOst();
+    },
+  };
 </script>
 
